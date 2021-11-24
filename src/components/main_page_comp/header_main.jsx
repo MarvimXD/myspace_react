3cbdf7d60f235fef680621d79db8ac3914577380
Victor Beserra <mxd46934@gmail.com>
2021-11-24 01:15:10 -0300

    Update header_main.jsx

--- src/components/main_page_comp/header_main.jsx
+++ src/components/main_page_comp/header_main.jsx
@@ -26,9 +26,9 @@ export default function Headermain() {
             
             <div className="header-menu-smartphone-display-container">
                 <div id="square-menu-id" className="header-menu-square">
-                    <img style={{ display: show ? "none" : "block" }} id="img-menu-smartphone-aux" src="img/comments.png" alt="" />
+                    <img style={{ display: show ? "none" : "block" }} id="img-menu-smartphone-aux" src="https://findicons.com/files/icons/129/soft_scraps/256/button_talk_balloon_01.png" alt="" />
                     <button id="btn-menu" onClick={showMenu}>
-                        <img id="img-menu-smartphone" src="img/help_robot.png" alt="" />
+                        <img id="img-menu-smartphone" src="https://lh3.googleusercontent.com/proxy/fbJEQJQM5es8Fzfn5foZBulNBGj1TzXZwrWFjBmaLa5apxUA8czRTLZ1nZarTq-h7dpJIOPxv0LEmGKGz9zStX4K-2OqJ2sl" alt="" />
                     </button>
                     <div className="header-menu-menu-options-container" style={{ marginLeft: show ? "-100em" : "0em" }}>
                     
